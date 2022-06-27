9aa5a07ea932c1acac2167c083a25bb7d9648479
reimima <reimima0916@gmail.com>
2022-06-27 19:36:36 +0900

    commit

---
+++ src/commands/help.ts
@@ -0,0 +1,33 @@
+import type { Bot } from '../Bot';
+import type { CommandInteraction } from 'discord.js';
+import { MessageEmbed } from 'discord.js';
+import { Command } from '../interfaces';
+
+export default class extends Command {
+    public constructor(public override readonly client: Bot) {
+        super(client, {
+            name: 'help',
+            description: 'Learn more about Embed Builder.',
+        });
+    }
+
+    public run(interaction: CommandInteraction<'cached'>) {
+        interaction.reply({ embeds: [new MessageEmbed()
+            .setTitle('About Embed Builder.')
+            .setDescription(`
+                Hi! First of all, thank you for introducing this bot.
+
+                I'm japanese, so maybe my english is bad. I'm sorry.
+
+                This bot has just been developed.
+
+                If you need help or have any ideas for Embed Builder, Join the support server!
+
+                I hope enjoy this bot!
+            `).addFields(
+                { name: 'author', value: 'reimima#3439' },
+                { name: 'support server', value: '[here]()' },
+                { name: 'github', value: '[here](https://github.com/reimima/embed-builder)' },
+            )] });
+    }
+}
