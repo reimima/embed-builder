f6ce6b0af4db55c5a93bad29abd341e05806854e
reimima <reimima0916@gmail.com>
2022-06-28 16:55:47 +0900

    commit

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -10,6 +10,8 @@ export default class extends Event {
         console.info('Succesfully logged in and is Ready.');
         console.info(`The bot is participating in ${this.client.guilds.cache.size ?? 'none'} servers.`);
 
+        this.client.user?.setActivity({ name: `Help by slash command.` });
+
         console.info('Starting to subscribe commands to Discord Server.');
         await this.client.commandManager.subscribe()
             .then(() => console.info('Succesfully subscribed commands to Discord Server.'))
